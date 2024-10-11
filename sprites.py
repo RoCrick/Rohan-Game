966e307a2fcb692f7efbbff3840dab64b78c3662
RoCrick <rohan.yarrakonda28@bcp.org>
2024-10-11 13:18:11 -0700

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -18,6 +18,7 @@ class Player(Sprite):
         self.y = y * TILESIZE
         self.speed = 10
         self.vx, self.vy = 0, 0
+        self.speed=1.5
     def get_keys(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
@@ -29,36 +30,49 @@ class Player(Sprite):
         if keys[pg.K_d]:
             self.vx += self.speed
     
-    
     def collide_with_walls(self, dir):
         if dir == 'x':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
             if hits:
                 if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
+                    self.x = hits[0].rect.left - TILESIZE
+                    # self.x = hits[0].rect.left - self.rect.width
                 if self.vx < 0:
                     self.x = hits[0].rect.right
                 self.vx = 0
                 self.rect.x = self.x
         if dir == 'y':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
-            
+            hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
             if hits:
                 if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
+                    self.y = hits[0].rect.top - TILESIZE
+                    # self.y = hits[0].rect.top - self.rect.height
                 if self.vy < 0:
                     self.y = hits[0].rect.bottom
                 self.vy = 0
                 self.rect.y = self.y
 
+    def collide_with_stuff(self, group, kill):
+        hits = pg.sprite.spritecollide(self, group, kill)
+        if hits:
+            if (hits[0].__class__.__name__) == "Powerup":
+                self.speed += 5
+                print("i hit a powerup...")
+
     def update(self):
         self.get_keys()
         self.x += self.vx * self.game.dt
         self.y += self.vy * self.game.dt
-        self.collide_with_walls('x')
+        # reverse order to fix collision issues
+        
+
+        self.collide_with_stuff(self.game.all_powerups, True)
+
         self.rect.x = self.x
-        self.collide_with_walls('y')
+        self.collide_with_walls('x')
+        
         self.rect.y = self.y
+        self.collide_with_walls('y')
 
 
 class Mob(Sprite):
@@ -68,7 +82,7 @@ class Mob(Sprite):
         Sprite.__init__(self, self.groups)
         self.image = pg.Surface((32, 32))
         self.rect = self.image.get_rect()
-        self.image.fill(YELLOW)
+        self.image.fill(GREEN)
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
         self.speed = 10
@@ -79,18 +93,21 @@ class Mob(Sprite):
         # moving towards the side of the screen
         self.rect.x += self.speed
         # when it hits the side of the screen, it will move in opposite direction
+
+        hits = pg.sprite.spritecollide(self, self.game.all_walls, False)
         
+        if hits:
+            # print("off the screen...")
+            self.speed *= -1
+            self.rect.y += 32
         if self.rect.right > WIDTH or self.rect.left < 0:
             # print("off the screen...")
-            
             self.speed *= -1
-            
             self.rect.y += 32
         # elif self.rect.colliderect(self.game.player):
         #     self.speed *= -1
         # elif self.rect.colliderect(self):
         #     self.speed *= -1
-
         
 
 class Wall(Sprite):
@@ -114,6 +131,6 @@ class Powerup(Sprite):
         Sprite.__init__(self, self.groups)
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.rect = self.image.get_rect()
-        self.image.fill(BLUE)
+        self.image.fill(YELLOW)
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
